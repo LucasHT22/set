5e01cd8195f10daeac87a84573e175b71e2829b2
Lucas Honda <88567622+LucasHT22@users.noreply.github.com>
2026-02-16 19:36:17 -0300

    changes

--- scripts/shape_drawer.gd
+++ scripts/shape_drawer.gd
@@ -1,19 +1,9 @@
 extends Control
 
-func _ready():
-	print("ShapeContainer ready! Size: ", size)
-
 func _draw():
-	print("ShapeContainer drawing! Size: ", size)
-	
 	var card = get_parent().get_parent().get_parent()
 	
-	if not card.has_method("get_card_properties"):
-		print("ERROR: Card doesn't have get_card_properties method!")
-		return
-	
 	var props = card.get_card_properties()
-	print("Drawing card with props: ", props)
 	
 	var shape_color = get_shape_color(props.card_color)
 	
@@ -26,7 +16,6 @@ func _draw():
 		var y_pos = y_offset + i * (shape_height + spacing)
 		var x_pos = (size.x - shape_width) / 2
 		
-		print("  Drawing shape ", i, " at position: ", Vector2(x_pos, y_pos))
 		draw_shape_at(Vector2(x_pos, y_pos), shape_width, shape_height, props.shape, shape_color, props.shading)
 
 func get_shape_color(card_color_enum):
@@ -39,18 +28,13 @@ func get_shape_color(card_color_enum):
 func draw_shape_at(pos: Vector2, width: float, height: float, shape_type, color: Color, shading_type):
 	var rect = Rect2(pos, Vector2(width, height))
 	
-	print("    draw_shape_at called: shape_type=", shape_type, " shading=", shading_type)
-	
-	# shape_type is an enum value (0, 1, 2)
 	match shape_type:
-		0:  # OVAL
+		0:
 			draw_oval_shape(rect, color, shading_type)
-		1:  # DIAMOND
+		1:
 			draw_diamond_shape(rect, color, shading_type)
-		2:  # SQUIGGLE
+		2:
 			draw_squiggle_shape(rect, color, shading_type)
-		_:
-			print("    ERROR: Unknown shape_type: ", shape_type)
 
 func draw_oval_shape(rect: Rect2, color: Color, shading_type):
 	var center = rect.position + rect.size / 2
@@ -66,14 +50,14 @@ func draw_oval_shape(rect: Rect2, color: Color, shading_type):
 		points.append(Vector2(x, y))
 	
 	match shading_type:
-		0:  # SOLID
+		0:
 			draw_colored_polygon(points, color)
-		1:  # STRIPED
+		1:
 			draw_colored_polygon(points, Color.WHITE)
 			for x in range(int(rect.position.x), int(rect.position.x + rect.size.x), 4):
 				draw_line(Vector2(x, rect.position.y), Vector2(x, rect.position.y + rect.size.y), color, 1.0)
 			draw_polyline(points + PackedVector2Array([points[0]]), color, 2.0)
-		2:  # OPEN
+		2:
 			draw_polyline(points + PackedVector2Array([points[0]]), color, 2.0)
 
 func draw_diamond_shape(rect: Rect2, color: Color, shading_type):
@@ -86,16 +70,83 @@ func draw_diamond_shape(rect: Rect2, color: Color, shading_type):
 	])
 	
 	match shading_type:
-		0:  # SOLID
+		0:
 			draw_colored_polygon(points, color)
-		1:  # STRIPED
+		1:
 			draw_colored_polygon(points, Color.WHITE)
 			for x in range(int(rect.position.x), int(rect.position.x + rect.size.x), 4):
 				draw_line(Vector2(x, rect.position.y), Vector2(x, rect.position.y + rect.size.y), color, 1.0)
 			draw_polyline(points + PackedVector2Array([points[0]]), color, 2.0)
-		2:  # OPEN
+		2:
 			draw_polyline(points + PackedVector2Array([points[0]]), color, 2.0)
 
 func draw_squiggle_shape(rect: Rect2, color: Color, shading_type):
-	# For now, use oval shape
-	draw_oval_shape(rect, color, shading_type)
+	var points = PackedVector2Array()
+	
+	var left_x = rect.position.x
+	var right_x = rect.position.x + rect.size.x
+	var top_y = rect.position.y
+	var bottom_y = rect.position.y + rect.size.y
+	var mid_y = rect.position.y + rect.size.y / 2
+	
+	var p0 = Vector2(left_x + rect.size.x * 0.2, top_y + rect.size.y * 0.3)
+	var p1 = Vector2(left_x + rect.size.x * 0.4, top_y)
+	var p2 = Vector2(right_x - rect.size.x * 0.4, top_y)
+	var p3 = Vector2(right_x - rect.size.x * 0.2, top_y + rect.size.y * 0.3)
+	
+	var steps = 20
+	for i in range(steps + 1):
+		var t = float(i) / steps
+		points.append(bezier_cubic(p0, p1, p2, p3, t))
+	
+	var r0 = p3
+	var r1 = Vector2(right_x, mid_y - rect.size.y * 0.1)
+	var r2 = Vector2(right_x, mid_y + rect.size.y * 0.1)
+	var r3 = Vector2(right_x - rect.size.x * 0.2, bottom_y - rect.size.y * 0.3)
+	
+	for i in range(1, steps + 1):
+		var t = float(i) / steps
+		points.append(bezier_cubic(r0, r1, r2, r3, t))
+	
+	var b0 = r3
+	var b1 = Vector2(right_x - rect.size.x * 0.4, bottom_y)
+	var b2 = Vector2(left_x + rect.size.x * 0.4, bottom_y)
+	var b3 = Vector2(left_x + rect.size.x * 0.2, bottom_y - rect.size.y * 0.3)
+	
+	for i in range(1, steps + 1):
+		var t = float(i) / steps
+		points.append(bezier_cubic(b0, b1, b2, b3, t))
+	
+	var l0 = b3
+	var l1 = Vector2(left_x, mid_y + rect.size.y * 0.1)
+	var l2 = Vector2(left_x, mid_y - rect.size.y * 0.1)
+	var l3 = p0
+	
+	for i in range(1, steps):
+		var t = float(i) / steps
+		points.append(bezier_cubic(l0, l1, l2, l3, t))
+	
+	match shading_type:
+		0:
+			draw_colored_polygon(points, color)
+		1:
+			draw_colored_polygon(points, Color.WHITE)
+			for x in range(int(rect.position.x), int(rect.position.x + rect.size.x), 4):
+				draw_line(Vector2(x, rect.position.y), Vector2(x, rect.position.y + rect.size.y), color, 1.5)
+			draw_polyline(points + PackedVector2Array([points[0]]), color, 2.0)
+		2:
+			draw_polyline(points + PackedVector2Array([points[0]]), color, 2.0)
+
+func bezier_cubic(p0: Vector2, p1: Vector2, p2: Vector2, p3: Vector2, t: float) -> Vector2:
+	var u = 1.0 - t
+	var tt = t * t
+	var uu = u * u
+	var uuu = uu * u
+	var ttt = tt * t
+	
+	var point = uuu * p0
+	point += 3 * uu * t * p1
+	point += 3 * u * tt * p2
+	point += ttt * p3
+	
+	return point
